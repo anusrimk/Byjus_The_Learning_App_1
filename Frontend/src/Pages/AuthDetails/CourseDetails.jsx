d205c338f1f1d1e79797170b01936ec59ac48dc3
Anusri Karmokar <anusrikarmokar@Anusris-MacBook-Air.local>
2024-12-20 00:12:16 +0530

    changed the css of all the components

--- Frontend/src/Pages/AuthDetails/CourseDetails.jsx
+++ Frontend/src/Pages/AuthDetails/CourseDetails.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./CourseDetails.module.css"
 
 function CourseDetails() {
   const nav = useNavigate();
